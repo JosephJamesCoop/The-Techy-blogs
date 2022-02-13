44f70ff65e7c74e878320a5b1e17cb6d4b735c91
JosephJamesCoop <cooper_3869@live.com>
2022-02-13 09:05:40 -0800

    fixed login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 
 router.get('/',(req,res) => {
-  User.findAll({})
+  User.findAll({
+    attributes: { exclude: ['password'] }
+  })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
       console.log(err);
@@ -12,6 +14,7 @@ router.get('/',(req,res) => {
 
 router.get('/:id', (req, res) => {
   User.findOne({
+    attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
     },
@@ -103,11 +106,44 @@ router.delete('/:id', (req, res) => {
 });
 
 router.post('/logout', (req, res) => {
- // need to add logout route
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
 });
 
 router.post('/login', (req, res) => {
-// need to add login route
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    // verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+   
+    req.session.save(() => {
+      // declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  });
 });
 
 module.exports = router;
