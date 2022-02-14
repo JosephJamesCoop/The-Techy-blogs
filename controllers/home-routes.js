a0a4ba802368f1515b71609f9d2a6030c8713566
JosephJamesCoop <cooper_3869@live.com>
2022-02-14 07:34:57 -0800

    setup homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,38 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  console.log(req.session)
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_text',
+      'title',
+      'created_at',
+    ],
+   
+  })
+    .then(dbPostData => {
+      // pass a single post object into the homepage template
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('homepage', { posts});
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+
 
 module.exports = router;
